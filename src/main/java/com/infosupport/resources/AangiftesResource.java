2c28b9fae9a6a2bb449cd4fd7ca97d0e126352d0
Bram Janssens <s.a.janssens@gmail.com>
2024-07-03 15:19:20 +0200

    POST and repo added

--- src/main/java/com/infosupport/resources/AangiftesResource.java
+++ src/main/java/com/infosupport/resources/AangiftesResource.java
@@ -1,23 +1,31 @@
 package com.infosupport.resources;
 
 import com.infosupport.domain.Aangifte;
+import com.infosupport.repositories.AangifteRepository;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
-@Path("aangiftes")
+@Path("aangiftes") // impliciet requestscoped
 public class AangiftesResource {
+
+    @Inject
+    private AangifteRepository repo;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Aangifte> getAll() {
-        return List.of(
-                new Aangifte("16.33.86.146", 200.00),
-                new Aangifte("16.33.86.147", 400.00));
+        return repo.findAll();
+    }
 
-        // return Response.ok(new Aangifte("16.33.86.146", 200.00)).build();
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Aangifte add(Aangifte aangifte) {
+        return repo.add(aangifte);
     }
 }
